d7c0b5c510283850a52e750b241e7f2957ddd095
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-04-14 11:49:08 +0100

    Provide alternative detection: deploy malware

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ cd /home/eval
     /ps "ps aux" \
     /rootkit ./bin/Defense_Evasion_via_Rootkit.sh \
     /masquerading ./bin/Defense_Evasion_via_Masquerading.sh \
-    /data_exfiltration ./bin/Exfiltration_via_Exfiltration_Over_Alternative_Protocol.sh
+    /data_exfiltration ./bin/Exfiltration_via_Exfiltration_Over_Alternative_Protocol.sh \
+    /deploy_malware ./bin/Malware_Linux_Trojan.sh
 
