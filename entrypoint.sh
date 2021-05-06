8f9306ba0a4ba5f05631ffd1d5b7b2f05631f0ff
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-05-06 14:55:52 +0100

    The file was renamed

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ sh -c echo CS_testcontainer starting
     /rootkit ./bin/Defense_Evasion_via_Rootkit.sh \
     /masquerading ./bin/Defense_Evasion_via_Masquerading.sh \
     /data_exfiltration ./bin/Exfiltration_via_Exfiltration_Over_Alternative_Protocol.sh \
-    /deploy_malware ./bin/Malware_Linux_Trojan.sh \
+    /deploy_malware ./bin/Malware_Linux_Trojan_Local.sh \
     /reverse_shell ./bin/Command_Control_via_Remote_Access.sh \
     /reverse_shell-obfuscated ./bin/Command_Control_via_Remote_Access-obfuscated.sh \
     /credentials_dumping ./bin/Credential_Access_via_Credential_Dumping.sh \
