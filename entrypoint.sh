3c513ab5a84bc2b3ed6b798110bc5cf6c5a7249f
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-11-23 14:04:57 +0000

    Replace old malware with the new one

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,14 @@ cd /home/eval
 # Mark as CS testcontainer
 sh -c echo CS_testcontainer starting
 
+chmod +x ./bin/Reverse_Shell_Trojan.sh
+
 exec /shell2http -show-errors -include-stderr \
     /ps "ps aux" \
     /rootkit ./bin/Defense_Evasion_via_Rootkit.sh \
     /masquerading ./bin/Defense_Evasion_via_Masquerading.sh \
     /data_exfiltration ./bin/Exfiltration_via_Exfiltration_Over_Alternative_Protocol.sh \
-    /deploy_malware './bin/Malware_Linux_Trojan_Local.sh' \
-    /deploy_malware2 './bin/Malware_Linux_Trojan_Remote.sh' \
+    /deploy_malware './bin/Reverse_Shell_Trojan.sh' \
     /reverse_shell ./bin/Command_Control_via_Remote_Access.sh \
     /reverse_shell-obfuscated ./bin/Command_Control_via_Remote_Access-obfuscated.sh \
     /credentials_dumping ./bin/Credential_Access_via_Credential_Dumping.sh \
